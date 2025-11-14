adf833fa9c70e946544f158a98c0b2da14d03a4a
mgueye <mguey2@illinois.edu>
2025-11-13 21:27:31 -0600

    Changed color of home bar to orange and Go button to blue.

--- app/src/main/java/com/example/functionalprototype/MainActivity.java
+++ app/src/main/java/com/example/functionalprototype/MainActivity.java
@@ -26,7 +26,6 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        // Handle system bars insets
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
